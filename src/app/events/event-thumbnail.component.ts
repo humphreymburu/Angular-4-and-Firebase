33b1f341ce55ce721c8ae932bd6fda73b8bbbd98
Humphrey <humphrey.asante@gmail.com>
2017-10-23 18:17:31 +0300

    added ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,7 +8,13 @@ import {
     <mat-card>
     <h2> {{event?.name}}</h2>
     <div>Date: {{event?.date}}</div>
-    <div>Date: {{event?.time}}</div>
+    <div [ngClass]="getStartTimeClass()" [ngSwitch] = "event?.time">
+    Time: {{event?.time}}
+    <span *ngSwitchCase="'8:00 am'">, Earley Start</span>
+    <span *ngSwitchCase="'10:00 am'">, Late Start</span>
+    <span *ngSwitchDafault> (normal-start)</span>
+
+    </div>
     <div>Price: \${{event?.price}}</div>
     <div *ngIf = event?.location>
     <span>Location:{{event?.location?.address}}</span>
@@ -16,15 +22,28 @@ import {
     <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
     </div>
     <div *ngIf = event?.onlineUrl>
-    Omline URL {{event?.onlineUrl}}
+    Online URL {{event?.onlineUrl}}
     </div>
     </mat-card>
     `,
     styles: [` 
-     .pad-left { margin-left: 10px;
-     }`]
+     .pad-left { 
+      margin-left: 10px;
+      }
+     .green {
+      color: #e91e63 !important;}  
+     .bold {
+         font-weight: bold;}
+    `]
 })
 
 export class EventsThumbnailComponent {
   @Input() event:any;
+
+  getStartTimeClass() {
+      const isEarly = this.event && this.event.time === '8:00 am';
+      return {green: isEarly,  bold: isEarly };
+      
+  }
+
 }
